37406cd04c2e6a73a4a1686527b2a2f2a601c924
derov81 <maniprutby@gmail.com>
2025-04-14 21:49:16 +0300

    Наяинаю восстанавливать утерянное, по своей тупости 14.04

--- homeprojectdrf/frontend/src/components/Sections/AdminPanel.js
+++ homeprojectdrf/frontend/src/components/Sections/AdminPanel.js
@@ -67,7 +67,6 @@ export default function AdminPanel() {
                 ))}
                 </tbody>
             </table>
-
         </section>
     )
 }
---
+++ homeprojectdrf/frontend/src/components/Sections/FeedbackSection.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+export default function FeedbackSection(){
+     
+    return(
+        <section>
+            <h3>Обратная связь</h3>
+
+        </section>
+    )
+}
